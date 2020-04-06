e5e6bca61d05637fa3efd836a77a7bf797f27666
Richard Williams <richard.williams@manchester.ac.uk>
2020-04-06 08:05:36 +0100

    Added sql queries

---
+++ data-extraction/sql-queries/template-standard.sql
@@ -0,0 +1,15 @@
+--Just want the output, not the messages
+SET NOCOUNT ON; 
+
+--{{SYMPTOM_LOWER_SPACED}}
+{{!MAIN}}
+PRINT 'Year,{{SYMPTOM_CAPITAL_NO_SPACE}}'
+select YEAR(EntryDate), count(*) as {{SYMPTOM_CAPITAL_NO_SPACE}} from (
+	select PatID, EntryDate from SIR_ALL_Records_Narrow
+	where ReadCode in ('{{CLINICAL_CODES}}')
+	and EntryDate >= '2000-01-01'
+	group by PatID, EntryDate
+) sub 
+group by EntryDate
+order by YEAR(EntryDate);
+{{MAIN}}
