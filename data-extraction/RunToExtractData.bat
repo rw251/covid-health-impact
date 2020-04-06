09faae8122ff5d47d5d3af8be5f5230922d08c7a
Richard Williams <richard.williams@manchester.ac.uk>
2020-04-06 08:27:02 +0100

    Add extract script

---
+++ data-extraction/RunToExtractData.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+REM move to batch dir 
+cd /d %~dp0
+
+SET SMASH.DB=PatientSafety_Records
+
+REM for each sql file execute against db
+forfiles /p sql-queries /s /m covid*.sql /c "cmd /c sqlcmd -E -d %SMASH.DB% -i @path -W -s , -h -1 -o @fname"
+
+REM each output file move to data directory and add a .txt extension
+forfiles /p sql-queries /s /m covid* /c "cmd /c if not @ext==0x22sql0x22 move /y @path ../data/@fname.txt"
+
+pause
