e27ad1030b6e4deec2fcd74572c7e12b49252980
Richard Williams <richard.williams@manchester.ac.uk>
2020-05-05 15:11:49 +0100

    Removed old data

--- data-extraction/RunToExtractData.bat
+++ data-extraction/RunToExtractData.bat
@@ -6,9 +6,9 @@ cd /d %~dp0
 SET SMASH.DB=PatientSafety_Records
 
 REM for each sql file execute against db
-forfiles /p sql-queries /s /m covid*.sql /c "cmd /c sqlcmd -E -d %SMASH.DB% -i @path -W -s , -h -1 -o @fname"
+forfiles /p sql-queries /s /m dx*.sql /c "cmd /c sqlcmd -E -d %SMASH.DB% -i @path -W -s , -h -1 -o @fname"
 
 REM each output file move to data directory and add a .txt extension
-forfiles /p sql-queries /s /m covid* /c "cmd /c if not @ext==0x22sql0x22 move /y @path ../data/@fname.txt"
+forfiles /p sql-queries /s /m dx* /c "cmd /c if not @ext==0x22sql0x22 move /y @path ../data/@fname.txt"
 
 pause
