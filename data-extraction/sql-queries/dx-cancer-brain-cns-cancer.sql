c5f744043d87087f9aa601d16f8350bb7126465f
Richard Williams <richard.williams@manchester.ac.uk>
2020-05-22 13:02:20 +0100

    New sql files - and slight change to how incidence and prevalence are calculated

--- data-extraction/sql-queries/dx-cancer-brain-cns-cancer.sql
+++ data-extraction/sql-queries/dx-cancer-brain-cns-cancer.sql
@@ -5,7 +5,7 @@ SET NOCOUNT ON;
 IF OBJECT_ID('tempdb..#AllDates') IS NOT NULL DROP TABLE #AllDates;
 CREATE TABLE #AllDates ([date] date);
 declare @dt datetime = '2015-01-01'
-declare @dtEnd datetime = '2020-05-21';
+declare @dtEnd datetime = '2020-05-22';
 WHILE (@dt <= @dtEnd) BEGIN
     insert into #AllDates([date])
         values(@dt)
@@ -17,7 +17,7 @@ END;
 select FirstDiagnosis, count(*) as num into #Incidence from (
 	select NHSNo, min(entrydate) as FirstDiagnosis from journal
 	where ReadCode in ('B51..00','B522.00','B525.00','B52y.00','BBb..00','BBb..99','BBb0.00','BBb0.11','BBb0.12','BBb1.00','BBb2.00','BBb2.11','BBb3.00','BBb3.11','BBb3.12','BBb6.00','BBb8.11','BBbB.00','BBbB.11','BBbB.12','BBbC.00','BBbD.00','BBbE.00','BBbE.11','BBbF.00','BBbG.00','BBbG.11','BBbG.12','BBbH.00','BBbJ.00','BBbK.00','BBbL.00','BBbL.11','BBbL.12','BBbM.00','BBbN.00','BBbP.00','BBbS.00','BBbT.00','BBbU.00','BBbV.00','BBbW.00','BBbX.00','BBbZ.00','BBba.00','BBbz.00','BBe7.00','ByuA.00','ByuA000','ByuA100','ByuA300','B51..','B522.','B525.','B52y.','BBb..','BBb0.','BBb1.','BBb2.','BBb3.','BBb6.','BBbB.','BBbC.','BBbD.','BBbE.','BBbF.','BBbG.','BBbH.','BBbJ.','BBbK.','BBbL.','BBbM.','BBbN.','BBbP.','BBbS.','BBbT.','BBbU.','BBbV.','BBbW.','BBbX.','BBbZ.','BBba.','BBbz.','BBe7.','ByuA.','ByuA0','ByuA1','ByuA3')
-	and entrydate <= '2020-05-21'
+	and entrydate <= '2020-05-22'
 	group by NHSNo
 ) sub 
 where FirstDiagnosis >= '2015-01-01'
@@ -29,7 +29,7 @@ select entrydate, count(*) as num into #Prevalence from (
 	select NHSNo, entrydate from journal
 	where ReadCode in ('B51..00','B522.00','B525.00','B52y.00','BBb..00','BBb..99','BBb0.00','BBb0.11','BBb0.12','BBb1.00','BBb2.00','BBb2.11','BBb3.00','BBb3.11','BBb3.12','BBb6.00','BBb8.11','BBbB.00','BBbB.11','BBbB.12','BBbC.00','BBbD.00','BBbE.00','BBbE.11','BBbF.00','BBbG.00','BBbG.11','BBbG.12','BBbH.00','BBbJ.00','BBbK.00','BBbL.00','BBbL.11','BBbL.12','BBbM.00','BBbN.00','BBbP.00','BBbS.00','BBbT.00','BBbU.00','BBbV.00','BBbW.00','BBbX.00','BBbZ.00','BBba.00','BBbz.00','BBe7.00','ByuA.00','ByuA000','ByuA100','ByuA300','B51..','B522.','B525.','B52y.','BBb..','BBb0.','BBb1.','BBb2.','BBb3.','BBb6.','BBbB.','BBbC.','BBbD.','BBbE.','BBbF.','BBbG.','BBbH.','BBbJ.','BBbK.','BBbL.','BBbM.','BBbN.','BBbP.','BBbS.','BBbT.','BBbU.','BBbV.','BBbW.','BBbX.','BBbZ.','BBba.','BBbz.','BBe7.','ByuA.','ByuA0','ByuA1','ByuA3')
 	and entrydate >= '2015-01-01'
-	and entrydate <= '2020-05-21'
+	and entrydate <= '2020-05-22'
 	group by NHSNo, entrydate
 ) sub 
 group by entrydate
--- data-extraction/utils.js
+++ data-extraction/utils.js
@@ -5,7 +5,8 @@ const CODESET_DIR = join(__dirname, 'codesets');
 
 const loadTemplate = () => readFileSync(join(__dirname, 'template.sql'), 'utf8');
 const loadGroupTemplate = () => readFileSync(join(__dirname, 'template-group.sql'), 'utf8');
-const loadGroupTemplatePart = () => readFileSync(join(__dirname, 'template-group-part.sql'), 'utf8');
+const loadGroupIncidenceTemplatePart = () => readFileSync(join(__dirname, 'template-group-incidence-part.sql'), 'utf8');
+const loadGroupPrevalenceTemplatePart = () => readFileSync(join(__dirname, 'template-group-prevalence-part.sql'), 'utf8');
 
 const codesFromFile = (pathToFile) => readFileSync(pathToFile, 'utf8')
   .split('\n')
@@ -64,18 +65,30 @@ const createQueriesFromDirectory = ({reportDateString, dirname}) => {
   files.forEach(filename => createQueryFromFile({reportDateString, filename, directory: subDirectory, group: nameDashed }));
   const mainQuery = codeFiles
     .map(x => {
-      let query = loadGroupTemplatePart();
+      let query = loadGroupIncidenceTemplatePart();
       query = query.replace(/\{\{CLINICAL_CODES\}\}/g, x.codeString);
       query = query.replace(/\{\{REPORT_DATE\}\}/g, reportDateString);
       query = query.replace(/\{\{NAME_LOWER_SPACED\}\}/g, x.nameLowerSpaced);
       return query;
     })
-    .join('\n UNION \n');
+    .join('\n UNION ALL \n'); 
+    // so that a person with condition 1 and condition 2 incident on same day counts as 2 towards total incidence 
+    // - would only be 1 if we used simply "UNION" which would remove duplicates
+  const prevalenceQuery = codeFiles
+    .map(x => {
+      let query = loadGroupPrevalenceTemplatePart();
+      query = query.replace(/\{\{CLINICAL_CODES\}\}/g, x.codeString);
+      query = query.replace(/\{\{REPORT_DATE\}\}/g, reportDateString);
+      query = query.replace(/\{\{NAME_LOWER_SPACED\}\}/g, x.nameLowerSpaced);
+      return query;
+    })
+    .join('\n UNION ALL \n'); 
   
   const allCodesCodeString = codeFiles.map(x => x.codeString).join("','")
     
   let groupQuery = loadGroupTemplate();
-  groupQuery = groupQuery.replace(/\{\{TEMPLATE_PARTS\}\}/g, mainQuery);
+  groupQuery = groupQuery.replace(/\{\{INCIDENCE_TEMPLATE_PARTS\}\}/g, mainQuery);
+  groupQuery = groupQuery.replace(/\{\{PREVALENCE_TEMPLATE_PARTS\}\}/g, prevalenceQuery);
   groupQuery = groupQuery.replace(/\{\{REPORT_DATE\}\}/g, reportDateString);
   groupQuery = groupQuery.replace(/\{\{ALL_CLINICAL_CODES\}\}/g, allCodesCodeString);
   groupQuery = groupQuery.replace(/\{\{NAME_CAPITAL_NO_SPACE\}\}/g, nameCapitalCase);
