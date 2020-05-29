f593f39a4272a4b5a4f6b16a868d2f45fb2f55f7
Richard Williams <richard.williams2@manchester.ac.uk>
2020-05-29 15:24:58 +0100

    Latest data from server

---
+++ data-extraction/RunToExtractData2.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+
+REM move to batch dir 
+cd /d %~dp0
+
+ECHO THIS WILL EXECUTE AGAINST PatientSafety_Records
+ECHO Are you sure you want to continue?
+pause
+
+SET SMASH.DB=PatientSafety_Records
+
+REM for each sql file execute against db
+forfiles /p sql-queries /s /m dx*.sql /c "cmd /c sqlcmd -E -d %SMASH.DB% -i @path -W -s , -h -1 -o @fname"
+
+REM each output file move to data directory and add a .txt extension
+forfiles /p sql-queries /s /m dx* /c "cmd /c if not @ext==0x22sql0x22 move /y @path ../data/@fname.txt"
+
+pause
