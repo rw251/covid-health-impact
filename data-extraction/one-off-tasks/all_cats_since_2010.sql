676e9e5c0d9d7b708cd8f9aad56f93d894fe74e0
Richard Williams <richard.williams2@manchester.ac.uk>
2020-06-03 15:22:14 +0100

    Better version of admin data which removes potential duplicates

--- data-extraction/one-off-tasks/all_cats_since_2010.sql
+++ data-extraction/one-off-tasks/all_cats_since_2010.sql
@@ -8,19 +8,117 @@ WHERE ReadCode like '[ABCDEFGHIJKLMNOPQRSTUVWXY]%'
 GROUP BY EntryDate
 ORDER BY EntryDate
 
+SELECT entrydate, count(*) from (
+select NHSNo, readcode, entrydate from DataFromSIR.dbo.journal
+WHERE ReadCode like '[ABCDEFGHIJKLMNOPQRSTUVWXY]%'
+	and ReadCode not like '[ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ]%'
+	and (len(ReadCode)=5 OR Len(ReadCode)=7)
+  and EntryDate >= '2010-01-01'
+  group by NHSNo, readcode, entrydate
+) sub
+GROUP BY entrydate
+ORDER BY entrydate
+
 -- Medications
 SELECT EntryDate, count(*) FROM DataFromSIR.dbo.journal
-WHERE ReadCode like '[abcdefghijklmnopqrstuvwxyz]%'
+WHERE (
+	ReadCode like '[abcdefghijklmnopqrstuvwxyz]%'
+	or (len(ReadCode)>7
+		and ReadCode not like 'PCSDT%'
+		and ReadCode not like 'EMIS%'
+		and ReadCode not like 'EGTON%'
+		and ReadCode not like 'ALLERG%'
+		and ReadCode not like 'CLEAT%'
+		and ReadCode not like 'FUNDHB%'
+		and ReadCode not like 'DEGRADE%'
+		and ReadCode not like '^%'
+		and ReadCode like '%[ABCDEFGHIJKLMNOPQRSTUVWXYZ]%')
+	or
+		(len(ReadCode)=7
+		and ReadCode not like 'PCSDT%'
+		and ReadCode not like 'EMIS%'
+		and ReadCode not like 'EGTON%'
+		and ReadCode not like 'ALLERG%'
+		and ReadCode not like 'CLEAT%'
+		and ReadCode not like 'FUNDHB%'
+		and ReadCode not like 'DEGRADE%'
+		and ReadCode like '[ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9]'
+		and ReadCode not like '%00')
+	or
+		(len(ReadCode)=6
+		and ReadCode not like 'PCSDT%'
+		and ReadCode not like 'EMIS%'
+		and ReadCode not like 'EGTON%'
+		and ReadCode not like 'ALLERG%'
+		and ReadCode not like 'CLEAT%'
+		and ReadCode not like 'FUNDHB%'
+		and ReadCode not like 'DEGRADE%'
+		and ReadCode like '[ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9]'
+		and ReadCode not like '%00')
+	)
   and EntryDate >= '2010-01-01'
 GROUP BY EntryDate
 ORDER BY EntryDate
 
+SELECT entrydate, count(*) from (
+select NHSNo, readcode, entrydate from DataFromSIR.dbo.journal
+WHERE (
+	ReadCode like '[abcdefghijklmnopqrstuvwxyz]%'
+	or (len(ReadCode)>7
+		and ReadCode not like 'PCSDT%'
+		and ReadCode not like 'EMIS%'
+		and ReadCode not like 'EGTON%'
+		and ReadCode not like 'ALLERG%'
+		and ReadCode not like 'CLEAT%'
+		and ReadCode not like 'FUNDHB%'
+		and ReadCode not like 'DEGRADE%'
+		and ReadCode not like '^%'
+		and ReadCode like '%[ABCDEFGHIJKLMNOPQRSTUVWXYZ]%')
+	or
+		(len(ReadCode)=7
+		and ReadCode not like 'PCSDT%'
+		and ReadCode not like 'EMIS%'
+		and ReadCode not like 'EGTON%'
+		and ReadCode not like 'ALLERG%'
+		and ReadCode not like 'CLEAT%'
+		and ReadCode not like 'FUNDHB%'
+		and ReadCode not like 'DEGRADE%'
+		and ReadCode like '[ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9]'
+		and ReadCode not like '%00')
+	or
+		(len(ReadCode)=6
+		and ReadCode not like 'PCSDT%'
+		and ReadCode not like 'EMIS%'
+		and ReadCode not like 'EGTON%'
+		and ReadCode not like 'ALLERG%'
+		and ReadCode not like 'CLEAT%'
+		and ReadCode not like 'FUNDHB%'
+		and ReadCode not like 'DEGRADE%'
+		and ReadCode like '[ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9][ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9]'
+		and ReadCode not like '%00')
+	)
+  and EntryDate >= '2010-01-01'
+  group by NHSNo, readcode, entrydate
+) sub
+GROUP BY entrydate
+ORDER BY entrydate
+
 -- Lab tests
 SELECT EntryDate, count(*) FROM DataFromSIR.dbo.journal
-WHERE ReadCode like '[4]%'
+WHERE ReadCode like '4%'
   and EntryDate >= '2010-01-01'
 GROUP BY EntryDate
 ORDER BY EntryDate
+
+SELECT entrydate, count(*) from (
+select NHSNo, readcode, entrydate from DataFromSIR.dbo.journal
+WHERE ReadCode like '4%'
+  and EntryDate >= '2010-01-01'
+  group by NHSNo, readcode, entrydate
+) sub
+GROUP BY entrydate
+ORDER BY entrydate
+
 
 -- Observations / symptoms
 SELECT EntryDate, count(*) FROM DataFromSIR.dbo.journal
@@ -29,12 +127,30 @@ WHERE ReadCode like '[12]%'
 GROUP BY EntryDate
 ORDER BY EntryDate
 
+SELECT entrydate, count(*) from (
+select NHSNo, readcode, entrydate from DataFromSIR.dbo.journal
+WHERE ReadCode like '[12]%'
+  and EntryDate >= '2010-01-01'
+  group by NHSNo, readcode, entrydate
+) sub
+GROUP BY entrydate
+ORDER BY entrydate
+
 -- Diag procedures / radiology
 SELECT EntryDate, count(*) FROM DataFromSIR.dbo.journal
 WHERE ReadCode like '[35]%'
   and EntryDate >= '2010-01-01'
 GROUP BY EntryDate
 ORDER BY EntryDate
+
+SELECT entrydate, count(*) from (
+select NHSNo, readcode, entrydate from DataFromSIR.dbo.journal
+WHERE ReadCode like '[35]%'
+  and EntryDate >= '2010-01-01'
+  group by NHSNo, readcode, entrydate
+) sub
+GROUP BY entrydate
+ORDER BY entrydate
 
 -- Procedures
 SELECT EntryDate, count(*) FROM DataFromSIR.dbo.journal
@@ -43,9 +159,28 @@ WHERE ReadCode like '[678]%'
 GROUP BY EntryDate
 ORDER BY EntryDate
 
+SELECT entrydate, count(*) from (
+select NHSNo, readcode, entrydate from DataFromSIR.dbo.journal
+WHERE ReadCode like '[678]%'
+  and EntryDate >= '2010-01-01'
+  group by NHSNo, readcode, entrydate
+) sub
+GROUP BY entrydate
+ORDER BY entrydate
+
 -- Admin
 SELECT EntryDate, count(*) FROM DataFromSIR.dbo.journal
 WHERE ReadCode like '[09]%'
   and EntryDate >= '2010-01-01'
 GROUP BY EntryDate
 ORDER BY EntryDate
+
+SELECT entrydate, count(*) from (
+select NHSNo, readcode, entrydate from DataFromSIR.dbo.journal
+WHERE ReadCode like '[09]%'
+  and EntryDate >= '2010-01-01'
+  group by NHSNo, readcode, entrydate
+) sub
+GROUP BY entrydate
+ORDER BY entrydate
+
