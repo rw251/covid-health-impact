cae1d925a568e949d5d6f7703f53df4ae5c846c7
Richard Williams <richard.williams@manchester.ac.uk>
2020-06-05 12:10:01 +0100

    Removed redundant code

--- Analysis/2010-present-time-series-analysis.R
+++ Analysis/2010-present-time-series-analysis.R
@@ -2,32 +2,6 @@ library("TTR")
 library("xts")
 library(ggfortify)
 
-daily <- read.delim("C:/Users/mdehsrw9/Development/covid-health-impact/data-extraction/one-off-tasks/all_RCs_bydate.txt", header=FALSE, col.names = c("Date","NumCodes"))
-daily$Date <- as.Date(as.character(daily$Date), format = "%Y%m%d") # read date variable as dates rather than text
-DF_daily <- xts(daily$NumCodes, order.by = daily$Date) 
-DF_weekly <- apply.weekly(DF_daily, FUN=sum)
-DF_weekly <- DF_weekly[-1,]
-DF_weekly <- head(DF_weekly, -2)
-weeklyTs <- ts(DF_weekly, frequency = 365.25/7, start = c(2010, 2))
-par(mfrow=c(2,1))
-plot.ts(weeklyTs, ylim=c(0,1200000))
-title("Number of administration codes each week 2010-present")
-abline(v = 2020.23, col = "red", lty = 3)
-text(2020.23, 30000, "lockdown", pos = 1, srt = 0)
-plot.ts(weeklyTs, ylim=c(0,1200000), xlim=c(2018,2021), xaxp=c(2018,2021,3))
-title("Number of administration codes each week 2018-present")
-abline(v = 2020.23, col = "red", lty = 3)
-text(2020.23, 30000, "lockdown", pos = 1, srt = 0)
-
-# sma14<-SMA(weeklyTs,n=7)
-# plot.ts(sma14)
-# 
-# comps<-decompose(weeklyTs)
-# plot(comps)
-# 
-# alltsseasonallyadjusted <- weeklyTs - comps$seasonal
-# plot(alltsseasonallyadjusted)
-
 ####### All
 daily <- read.delim("C:/Users/mdehsrw9/Development/covid-health-impact/data-extraction/one-off-tasks/all_since_2010.txt", header=FALSE, col.names = c("Date","NumCodes"))
 daily$Date <- as.Date(as.character(daily$Date), format = "%Y%m%d") # read date variable as dates rather than text
